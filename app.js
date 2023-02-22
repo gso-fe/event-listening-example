6f72bc55af4a5f03cfee8aa1efb7a74fbd6dfcce
gso-fe <110996927+gso-fe@users.noreply.github.com>
2023-02-22 22:35:53 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
-const addTask = document.querySelector('.add');
-const taskList = document.querySelector('.tasks');
+const addTask = document.querySelector('.add-task');
+const taskList = document.querySelector('.task-list');
 const deleteTask = document.querySelector('ul');
 
-const generateListTemplate = newTask => {
-  const html = `
+const generateListEntry = newTask => {
+  const html_template = `
     <li class="list-group-item d-flex justify-content-between align-items-center">
       <span>${newTask}</span>
-      <i class="far fa-trash-alt deleteTask"></i>
+      <i class="far fa-trash-alt delete-task"></i>
     </li>`;
-  taskList.innerHTML += html;
+  taskList.innerHTML += html_template;
 };
 
 addTask.addEventListener('submit', e => {
   e.preventDefault();
   const newTask = addTask.add.value.trim();
-  generateListTemplate(newTask);
+  generateListEntry(newTask);
 });
 
 deleteTask.addEventListener('click', e => {
-  console.log(e);
-  if(e.target.classList.contains('deleteTask')) {
+  // console.log(e);
+  if(e.target.classList.contains('delete-task')) {
     e.target.parentElement.remove();
   }
 });
